15c25bb629454434dd12439729834eda57c3df14
Jebeandiah <jebeandiah@gmail.com>
2026-09-07 17:01:51 -0700

    formatting

--- fpga/src/heartbeat.sv
+++ fpga/src/heartbeat.sv
@@ -13,27 +13,25 @@ output logic fpga_blink_out);
 	
 	// Simple clock divider
 	always_ff @(posedge clk)
+	begin
+		if(reset == 0) 
 		begin
-			if(reset == 0) begin
-				fpga_blink_out <=0;
-				counter <=0;
-				end
-						else if(counter == MAX_COUNT) 
-				counter <= 0;
-
-			else
-			begin
+			fpga_blink_out <=0;
+			counter <=0;
+		end
+		else if(counter == MAX_COUNT) 
+			counter <= 0;
+		else
+		begin
 			if(counter == 0) 
 				fpga_blink_out <= 0;
 				
 			else if(counter == MAX_COUNT/2)
 				fpga_blink_out <= 1;
 				
-
-				
 			if(enable == 1)
 				counter <= counter+1;
-			end
 		end
+	end
 		
 endmodule
